d8fd28e9c5f62dc18909bd80358dc3bba5994561
Michael Schaaf <Maschaaf@hotmail.de>
2022-07-03 09:46:53 +0200

    added min function

--- app.js
+++ app.js
@@ -89,5 +89,15 @@ function myArrayMax(arr) {
 } 
 
 
-
+Example (Find Min)
+function myArrayMin(arr) {
+  let len = arr.length;
+  let min = Infinity;
+  while (len--) {
+    if (arr[len] < min) {
+      min = arr[len];
+    }
+  }
+  return min;
+} 
 */
